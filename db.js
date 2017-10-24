8c892fe9c073995ebe08c5b5aa5114711e73dfb3
Koti Bajjuri <kotireddy.b@gmail.com>
2017-10-24 17:27:58 -0600

    Updated DB

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-mongoose.connect("mongodb://userName:<<Password>>@host1:27017,host2:27017,host3:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin", 
+mongoose.connect("mongodb://<<userName>>:<<Password>>@host1:27017,host2:27017,host3:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin", 
 {useMongoClient: true},
 function(err) {
     if(err) console.log("Error Connecting Database: "+err);
